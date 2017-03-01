381139a2b1dbeadb3d7928b863d01526493b28d4
afaf <shoog-alroo7_@hotmail.com>
2017-03-01 15:02:35 +0300

    Layout for business add event

--- app/src/main/java/com/example/afaf/newlayout/BusinessAddEvent.java
+++ app/src/main/java/com/example/afaf/newlayout/BusinessAddEvent.java
@@ -1,13 +1,71 @@
 package com.example.afaf.newlayout;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import java.io.File;
 
 public class BusinessAddEvent extends AppCompatActivity {
-
+    private Uri imageCaptureUri;
+    private static final int PICK_FROM_CAMERA = 1;
+    private static final int PICK_FROM_FILE = 2;
+    Button btn_choose_image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_business_add_event);
+        final String[] items = new String[]{"camera", "Album"};
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.select_dialog_item, items);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Select Image From:");
+        builder.setAdapter(adapter, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (which == 0) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    File file = new File(Environment.getExternalStorageDirectory(), "tmp_avatar" + String.valueOf(System.currentTimeMillis()) + ".jpg");
+                    imageCaptureUri = Uri.fromFile(file);
+                    try {
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageCaptureUri);
+                        intent.putExtra("return data", true);
+
+                        startActivityForResult(intent, PICK_FROM_CAMERA);
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                    dialog.cancel();
+                } else {
+                    Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(Intent.createChooser(intent, "Complete action using "), PICK_FROM_FILE);
+                }
+
+            }
+        });
+        final AlertDialog dialog = builder.create();
+        ImageButton btn_choose_image = (ImageButton) findViewById(R.id.btn_choose_image);
+        btn_choose_image.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                dialog.show();
+
+            }
+        });
     }
+
+
 }
+
+
